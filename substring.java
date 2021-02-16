5b371beb7348080f0a77e89b3fbcb6efe99629be
vaish077 <54187628+vaish077@users.noreply.github.com>
2021-02-16 13:37:56 +0530

    Solution

---
+++ substring.java
@@ -0,0 +1,7 @@
+public class substring{
+    public static void main(String[] args){
+        String str = "Naman Cute";
+        System.out.println(str.replace('N' , 'C'));
+        System.out.println(str.replaceFirst("Na" , "Ce"));
+    }
+}
